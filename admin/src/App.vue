709579ff5d441128ec16670f74aa1f242778b09f
MyFaith <854372534@qq.com>
2019-12-28 11:58:48 +0800

    :construction: Page basic layout and menu

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,7 +1,29 @@
 <template>
     <div id="app">
-        <router-view />
+        <layout>
+            <router-view />
+        </layout>
     </div>
 </template>
 
-<style lang="scss"></style>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Layout from './components/layout.vue';
+
+@Component({
+    components: {
+        Layout
+    }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+body {
+    margin: 0;
+    padding: 0;
+}
+* {
+    box-sizing: border-box;
+}
+</style>
---
+++ admin/src/components/layout.vue
@@ -0,0 +1,65 @@
+<template>
+    <el-container class="container">
+        <el-aside class="left-side">
+            <el-menu default-active="1" class="menu" router>
+                <el-submenu :index="`menu-${i}`" v-for="(menu, i) in menuList" :key="i">
+                    <template slot="title">
+                        <i v-if="menu.icon" :class="menu.icon"></i>
+                        <span>{{ menu.title }}</span>
+                    </template>
+                    <el-menu-item :index="item.path" v-for="(item, idx) in menu.items" :key="idx">{{ item.title }}</el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <el-header class="header">Header</el-header>
+            <el-main class="content">
+                <slot></slot>
+            </el-main>
+            <el-footer class="footer">Footer</el-footer>
+        </el-container>
+    </el-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Layout extends Vue {
+    menuList = [
+        {
+            title: '菜单1',
+            icon: 'el-icon-user',
+            items: [
+                { title: '菜单1-1', path: '/' },
+                { title: '菜单1-1', path: '/' }
+            ]
+        },
+        {
+            title: '菜单2',
+            icon: 'el-icon-location',
+            items: [
+                { title: '菜单2-1', path: '/1' },
+                { title: '菜单2-1', path: '/2' },
+                { title: '菜单2-3', path: '/3' },
+                { title: '菜单2-4', path: '/4' }
+            ]
+        }
+    ];
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    .left-side {
+        .menu {
+            height: 100vh;
+        }
+    }
+    .header,
+    .footer {
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
