5bc7a7dca83da6f11b46d130cf40188a6dc96e2c
MyFaith <854372534@qq.com>
2019-12-30 11:37:32 +0800

    :bug: Fix router problem

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,4 @@
 import Koa from 'koa';
-import Router from 'koa-router';
 import Mongoose from 'mongoose';
 import Config from './config';
 import routers from './routers';
@@ -9,8 +8,7 @@ import './models';
 Mongoose.connect(Config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = new Koa();
-const router = new Router();
-router.use(routers.routes());
+app.use(routers.routes());
 
 app.listen(8004, () => {
     console.log('Started => http://localhost:8004');
