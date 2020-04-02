03ff23087624ceb91a71c47a7b66cb53b2dfd792
MyFaith <854372534@qq.com>
2020-04-02 15:02:15 +0800

    🚧 Playlist and search playlist

--- web/src/components/playlist.vue
+++ web/src/components/playlist.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="playlist">
-        <a-collapse accordion>
-            <a-collapse-panel header="A">
-                <template>
-                    <a-collapse accordion>
-                        <a-collapse-panel header="AA">
-                            <p>111</p>
-                        </a-collapse-panel>
-                    </a-collapse>
-                </template>
-            </a-collapse-panel>
+        <a-form layout="horizontal">
+            <a-form-item label="搜索" :labelCol="{ span: 3 }" :wrapperCol="{ span: 20 }">
+                <a-input-search v-model="searchText" @search="search" enterButton></a-input-search>
+            </a-form-item>
+        </a-form>
+        <a-collapse>
+          <a-collapse-panel class="category" v-for="(category, key) in source" :key="key" :header="key">
+              <a-list class="source-list">
+                <a-list-item class="source-item" @click="switchChannel(source)" v-for="(source, idx) in category" :key="idx">
+                    {{ source.name }}
+                </a-list-item>
+              </a-list>
+          </a-collapse-panel>
         </a-collapse>
     </div>
 </template>
@@ -18,58 +21,69 @@
 export default {
     data() {
         return {
-            category: []
+            searchText: '',
+            source: {},
+            originalSource: {}
         };
     },
+    methods: {
+        switchChannel(data) {
+            this.$emit('switchChannel', data);
+        },
+        search() {
+            const _this = this;
+            if (_this.searchText) {
+                _this.originalSource = { ..._this.source };
+                Object.keys(_this.source).forEach(key => {
+                    const data = _this.source[key].filter(e => {
+                        return e.name.indexOf(_this.searchText) !== -1
+                    });
+                    _this.source[key] = data;
+                });
+            } else {
+                if (Object.keys(_this.originalSource).length > 0) _this.source = _this.originalSource;
+            }
+        },
+        getData() {
+            const params = { page: 0, size: 0 }
+            this.$http.get('/source', { params }).then(res => {
+                this.source = res.data.list.reduce(function (obj, item) {
+                    var key = item['category']['name'];
+                    if (!obj.hasOwnProperty(key)) {
+                        obj[key] = [];
+                    }
+                    obj[key].push(item);
+                    return obj;
+                }, {});
+            });
+        }
+    },
     created() {
-        this.$http.get('/category').then(res => {
-            this.category = res.data.list.reduce((arr, item) => {
-                if (item.parent) {
-                    const parentObj = arr.find(e => e._id === item.parent);
-                    if (parentObj) {
-                        parentObj.children.push(item);
-                    } else {
-                        const tmp = res.data.list.find(e => e._id === item.parent);
-                        arr.push({ ...tmp, children: [item] });
-                    }
-                } else {
-                    if (!arr.find(e => e._id === item._id)) {
-                        arr.push({ ...item, children: [] });
-                    }
-                }
-                return arr;
-            }, []);
-        });
+        this.getData();
     }
 };
 </script>
 
 <style lang="scss" scoped>
 .playlist {
-    margin: 50px auto;
-    width: 30%;
+    margin: 80px auto;
+    width: 20%;
+    .ant-list-item {
+        padding: 12px;
+    }
+    .source-item {
+        cursor: pointer;
+        &:hover {
+            background: #f3f3f3;
+        }
+    }
     ::v-deep .ant-collapse {
-        background: transparent;
-        border: 0;
         .ant-collapse-item {
-            background: #424242;
-            border: 0;
-            border-radius: 0;
-            .ant-collapse-header {
-                border: 0;
-                border-radius: 0;
-            }
             .ant-collapse-content {
-                border: 0;
                 .ant-collapse-content-box {
-                    background: #424242;
-                    border: 0;
+                    padding: 0;
                 }
             }
-        }
-
-        .ant-collapse-item > .ant-collapse-header:hover {
-            background: #585858;
         }
     }
 }
